adf306122ddca72873caf66e0bc6a0f8e7c765a3
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-05-24 12:36:59 -0700

    commit tree f8b5b65e47aa00eac99b6830356b3709108ca66f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :item_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+    else
+      item = Item.create(item_params)
+    end
+    render json: item, status: :created
+  end  
+
+  private
+
+  def item_not_found
+    render json: {error: "Item not found"}, status: 404
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
